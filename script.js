955ac3f8cefe91862390a18a0d0e6f5c4c686f91
INTESARSHAWON <intesarshawon@gmail.com>
2024-04-21 20:00:43 +0600

    main issue

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("one");
+console.log("two");
+
+setTimeout(()=>{
+    console.log("hello");
+},2000);
+//setTimeout(hello,5000);
+
+// eta timeout function, eta emne likha jay, omneo likha jay
+// 5 sec=5000 mili second
+// etar kaj 5 second por print hocche
+
+console.log("three");
+console.log("four");
