bec4fdb4adf6cc0a521fadfc61edc08d0cd8288e
INTESARSHAWON <intesarshawon@gmail.com>
2024-04-22 21:05:34 +0600

    callback function

--- script.js
+++ script.js
@@ -12,3 +12,23 @@ setTimeout(()=>{
 
 console.log("three");
 console.log("four");
+
+
+
+//callback function- eta emn ekta function jeta onno function er moddhe argument hishebe pass hoy
+
+
+function sum(a,b){
+    console.log (a+b);
+}
+
+function calculator (a,b,sumCallback) {
+    sumCallback(a,b);
+}
+
+calculator(1,2,sum);
+
+//call back a ami jokhon e function pass korbo, eta (), mane parenthrsis dea jabe na, tahole run korbe na
+// sudhu nam likhle run korbe
+//calculator(1,2,sum()); jmn eta jodi run korai kaj korbe na
+//prothom example a jeta ache sekhaneo set callback function chilo, //setTimeout(hello,5000)- eta chilo
